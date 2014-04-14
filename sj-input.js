96b91fa99b7847e68b0dec9dcd1996b4e5e48ef5
slopjong <git@slopjong.de>
2014-04-14 18:18:35 +0200

    Requiring ngModel, no scoping

--- sj-input.js
+++ sj-input.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/* QUICK AND DIRTY IMPLEMENTATION WITH UNPROPER SCOPING */
-
 // Original jQuery code by James Padolsey
 // Ported to AngularJS by Romain Schmitz
 // Directive based on http://stackoverflow.com/a/931695
@@ -9,19 +7,16 @@ angular
   .module("sj.input", [])
   .directive("sjInput", function($compile) {
     return {
-      restrict: 'EA',
-      template: '<input type="text" ng-model="text">',
-      replace: true,
-      link: function(scope, element, attributes) {
+      restrict: 'A',
+      require: '?ngModel',
+      link: function(scope, element, attributes, ngModel) {
 
         // default settings
-        scope.config = {
+        var config = {
           maxWidth: 1000,
           minWidth: 0,
           comfortZone: 25
         };
-
-        scope.text = "";
 
         function computed(element) {
           // get the element's computed style
@@ -30,11 +25,11 @@ angular
         }
 
         var style = computed(element);
-        var minWidth = scope.config.minWidth || parseInt(style.width);
+        var minWidth = config.minWidth || parseInt(style.width);
 
         // create a new test element with the same style as the input element
-        scope.testSubject = $compile('<sj-input-tester></je-input-tester>')(scope);
-        scope.testSubject.css({
+        var testSubject = $compile('<sj-input-tester></je-input-tester>')(scope);
+        testSubject.css({
           position: 'absolute',
           top: -9999 + 'px',
           left: -9999 + 'px',
@@ -50,21 +45,21 @@ angular
 
           // escape the text to avoid wrong rendering because of wrong
           // browser encoding settings
-          var escaped = scope.text
+          var escaped = ngModel.$viewValue
             .replace(/&/g, '&amp;')
             .replace(/\s/g,'&nbsp;')
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;');
 
-          scope.testSubject.html(escaped);
+          testSubject.html(escaped);
 
           // Calculate new width + whether to change
-          var testerWidth = parseInt(computed(scope.testSubject).width);
+          var testerWidth = parseInt(computed(testSubject).width);
 
           var newWidth;
 
-          if ( (testerWidth + scope.config.comfortZone) >= minWidth) {
-            newWidth = testerWidth + scope.config.comfortZone;
+          if ( (testerWidth + config.comfortZone) >= minWidth) {
+            newWidth = testerWidth + config.comfortZone;
           } else {
             newWidth = minWidth;
           }
@@ -72,14 +67,14 @@ angular
           var currentWidth = parseInt(computed(element).width);
 
           var isValidWidthChange = (newWidth < currentWidth && newWidth >= minWidth) ||
-            (newWidth > minWidth && newWidth < scope.config.maxWidth);
+            (newWidth > minWidth && newWidth < config.maxWidth);
 
           if (isValidWidthChange) {
             element.css('width', newWidth + 'px');
           }
         };
 
-        element.after(scope.testSubject);
+        element.after(testSubject);
 
         element.on('keyup keydown blur update', function() {
           check();
